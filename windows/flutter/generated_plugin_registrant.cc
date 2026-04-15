75d2133b978a08d40075f65cb56deb67cb74bb5e
Linda <linda.tech24@gmail.com>
2026-04-15 19:37:11 +0530

    Remove unused skeleton components, project and service models, and various widgets
    - Deleted `discover_categories_skelton.dart`, `offers_skelton.dart`, and `skelton.dart` files.
    - Removed `project_model.dart` and `service_model.dart` files, including their associated classes and methods.
    - Eliminated `circular_progress_ring.dart`, `error_state_widget.dart`, `loading_state_widget.dart`, `project_module_card.dart`, and `shimmer_loading.dart` files.
    - Updated `pubspec.yaml` and `pubspec.lock` to remove dependencies related to the deleted files.
    - Adjusted Windows plugin registration to exclude `permission_handler_windows`.

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -11,7 +11,6 @@
 #include <flutter_inappwebview_windows/flutter_inappwebview_windows_plugin_c_api.h>
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 #include <geolocator_windows/geolocator_windows.h>
-#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <share_plus/share_plus_windows_plugin_c_api.h>
 #include <syncfusion_pdfviewer_windows/syncfusion_pdfviewer_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
@@ -27,8 +26,6 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
-  PermissionHandlerWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   SharePlusWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
   SyncfusionPdfviewerWindowsPluginRegisterWithRegistrar(
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,7 +8,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   flutter_inappwebview_windows
   flutter_secure_storage_windows
   geolocator_windows
-  permission_handler_windows
   share_plus
   syncfusion_pdfviewer_windows
   url_launcher_windows
